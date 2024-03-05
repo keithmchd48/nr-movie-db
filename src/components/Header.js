a07d3b038c24155573096ff82bec1b0b5a8fdfcc
keithmchd48 <keithmchd48@gmail.com>
2024-03-05 21:35:31 +0100

    profile dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {MAIN_LOGO, AVATAR} from '../utils/assets';
 import { useNavigate } from 'react-router-dom';
 import auth from '../utils/firebase';
@@ -10,11 +10,16 @@ const Header = () => {
   const navigate = useNavigate();
   const user = useSelector(store => store.user);
   const dispatch = useDispatch();
+  const [isProfileOptionsOpen, setIsProfileOptionsOpen] = useState(false);
   
   const handleLogout = () => {
     signOut(auth).catch((error) => {
       console.log(error.message);
     });
+  };
+
+  const openProfileOptions = () => {
+    setIsProfileOptionsOpen(!isProfileOptionsOpen);
   };
 
   useEffect(() => {
@@ -37,15 +42,19 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="px-2 py-2 sm:py-4 flex justify-between items-center">
-      <img alt="main_logo" src={MAIN_LOGO} className="h-16 sm:h-20"></img>
+    <div className="px-2 flex justify-between items-center">
+      <img alt="main_logo" src={MAIN_LOGO} className="h-16"></img>
       {user && (
-        <div className="flex flex-col items-end">
-          <img alt="avatar" src={user.photoURL || AVATAR} className="h-8 w-8 sm:h-8 sm:w-8"></img>
-          {user.displayName && <p className="text-white text-sm">{user.displayName}</p>}
-          <div className="text-right">
-            <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded-lg">Sign out</button>
-          </div>
+        <div className="relative">
+          <img onClick={openProfileOptions} alt="avatar" src={user.photoURL || AVATAR} className="h-8 w-8 sm:h-8 sm:w-8 cursor-pointer"></img>
+            {isProfileOptionsOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-black shadow-lg text-white border-[1px] border-gray-400 opacity-80">
+                <div className="p-2">
+                  {user.displayName && <p>{user.displayName}</p>}
+                  <button onClick={handleLogout} className="block w-full text-left py-2 hover:underline">Sign out</button>
+                </div>
+              </div>
+            )}
         </div>
       )}
     </div>
