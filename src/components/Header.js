d5ed69eb0a32b18d65cfffd9f5e64b66b4d8e222
keithmchd48 <keithmchd48@gmail.com>
2024-03-09 13:24:56 +0100

    click outside mplemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,33 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {MAIN_LOGO, AVATAR} from '../utils/assets';
 import { useNavigate } from 'react-router-dom';
 import auth from '../utils/firebase';
 import {signOut, onAuthStateChanged} from 'firebase/auth';
 import { useSelector, useDispatch } from 'react-redux';
 import { LOGOUT_USER, ADD_USER } from '../utils/slices/userSlice';
+import useClickOutside from '../hooks/useClickOutside';
 
 const Header = () => {
   const navigate = useNavigate();
   const user = useSelector(store => store.user);
   const dispatch = useDispatch();
   const [isProfileOptionsOpen, setIsProfileOptionsOpen] = useState(false);
+  const profileDropdownRef = useRef(null);
+
+  useClickOutside(profileDropdownRef, () => {
+    if (isProfileOptionsOpen) {
+      setIsProfileOptionsOpen(false);
+    }
+  });
+
+  const toggleProfileOptions = () => {
+    setIsProfileOptionsOpen(prev => !prev);
+  };
   
   const handleLogout = () => {
     signOut(auth).catch((error) => {
       console.log(error.message);
     });
-  };
-
-  const openProfileOptions = () => {
-    setIsProfileOptionsOpen(prev => !prev);
   };
 
   useEffect(() => {
@@ -45,18 +53,16 @@ const Header = () => {
     <div className="px-2 flex justify-between items-center bg-gradient-to-b from-black">
       <img alt="main_logo" src={MAIN_LOGO} className="h-16"></img>
       {user && (
-        <div className="relative">
-          <button onClick={openProfileOptions} tabIndex="0">
+        <div ref={profileDropdownRef} className="relative">
+          <button onClick={toggleProfileOptions} tabIndex="0">
             <img alt="avatar" src={user.photoURL || AVATAR} className="h-8 w-8 sm:h-8 sm:w-8 cursor-pointer"></img>
           </button>
-          {isProfileOptionsOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-black shadow-lg text-white border-[1px] border-gray-400 opacity-80">
-              <div className="p-2">
-                {user.displayName && <p>{user.displayName}</p>}
-                <button onClick={handleLogout} className="block w-full text-left py-2 hover:underline">Sign out</button>
-              </div>
+          <div className={`absolute right-0 mt-2 w-48 bg-black shadow-lg text-white border-[1px] border-gray-400 opacity-80 ${isProfileOptionsOpen ? 'block' : 'hidden'}`}>
+            <div className="p-2">
+              {user.displayName && <p>{user.displayName}</p>}
+              <button onClick={handleLogout} className="block w-full text-left py-2 hover:underline">Sign out</button>
             </div>
-          )}
+          </div>
         </div>
       )}
     </div>
---
+++ src/hooks/useClickOutside.js
@@ -0,0 +1,9 @@
+import useEventListener from "./useEventListener";
+
+export default function useClickOutside(ref, callback) {
+  useEventListener("click", (e) => {
+    if (ref.current == null || ref.current.contains(e.target)) return;
+
+    callback(e);
+  }, document);
+};
