cb04f09da2a77551fa7cd3634a66c993c7af1609
keithmchd48 <keithmchd48@gmail.com>
2024-03-10 16:17:52 +0100

    api constants and route constant added

--- src/components/SampleCard.js
+++ src/components/SampleCard.js
@@ -2,7 +2,7 @@ import {POSTER_PATH_URL} from '../utils/assets';
 
 const SampleCard = ({sample}) => {
   return (
-    <div className="w-60 mx-3 first:ml-0 last:mr-0">
+    <div className="w-60 mx-1 first:ml-0 last:mr-0">
       <img src={`${POSTER_PATH_URL}${sample.backdrop_path}`} alt="sample_poster" className="rounded-md" />
     </div>
   );
