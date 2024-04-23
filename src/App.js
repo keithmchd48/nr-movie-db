cc09b67f759a4092d8f73d9f8307e5cb471d9c44
keithmchd48 <keithmchd48@gmail.com>
2024-04-23 20:07:22 +0200

    persist reducers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Body from "./components/Body";
 import appStore from "./utils/appStore";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 function App() {
+  const persistor = persistStore(appStore);
   return (
     <Provider store={appStore}>
-      <div className="font-Roboto bg-brand-black w-full h-full">
-        <Body />
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="font-Roboto bg-brand-black w-full h-full">
+          <Body />
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -6,15 +6,33 @@ import trailerReducer from "./slices/trailerSlice";
 import searchReducer from "./slices/searchSlice";
 import configReducer from "./slices/configSlice";
 
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducers = combineReducers({
+  user: userReducer,
+  movies: movieReducer,
+  tvShows: tvShowReducer,
+  trailer: trailerReducer,
+  search: searchReducer,
+  config: configReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const appStore = configureStore({
-  reducer: {
-    user: userReducer,
-    movies: movieReducer,
-    tvShows: tvShowReducer,
-    trailer: trailerReducer,
-    search: searchReducer,
-    config: configReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default appStore;
