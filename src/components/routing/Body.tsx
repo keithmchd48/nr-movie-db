d430e6ff13fd394f407e12e68475a84f9b43ee0d
keithmchd48 <keithmchd48@gmail.com>
2025-04-07 21:27:17 +0200

    navigate to login

--- src/components/routing/Body.tsx
+++ src/components/routing/Body.tsx
@@ -1,5 +1,5 @@
 import ProtectedRoute from "components/routing/ProtectedRoute";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import { PATHS } from "utils/assets";
 import { lazy } from 'react';
 
@@ -11,7 +11,7 @@ const AuthPage = lazy(() => import("pages/AuthPage"))
 const Body = () => {
   const appRouter = createBrowserRouter([
     { path: PATHS.LOGIN, element: <AuthPage /> },
-    { path: PATHS.AUTH, element: <AuthPage /> },
+    { path: PATHS.AUTH, element: <Navigate to={PATHS.LOGIN} replace /> },
     {
       element: <ProtectedRoute />,
       children: [
