43da7d5442c72d37c2c58d14de3b0acefa63c95b
keithmchd48 <keithmchd48@gmail.com>
2025-04-13 11:56:06 +0200

    auth state hook

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useRef, RefObject } from "react";
-import { PATHS } from "utils/assets";
-import { useNavigate } from "react-router-dom";
-import auth from "utils/firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { LOGOUT_USER, ADD_USER, TUser } from "store/slices/userSlice";
-import { UPDATE_SEARCH_QUERY } from "store/slices/searchSlice";
+import { useRef, RefObject } from "react";
+import { TUser } from "store/slices/userSlice";
 import { TOGGLE_HAMBURGER_MENU } from "store/slices/configSlice";
 import ProfileDropdown from "components/profile/ProfileDropdown";
 import SearchComponent from "components/search/SearchComponent";
 import { useSelector, useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom";
 import LangSelect from "components/translations/LangSelect";
 import ScrollContainer from "components/header/ScrollContainer";
 import HeaderMenu from "components/header/HeaderMenu";
@@ -21,14 +15,20 @@ import MainLogo from "components/units/MainLogo";
 import useClickOutside from "hooks/utilities/useClickOutside";
 import useDocumentTitle from "hooks/useDocumentTitle";
 import { RootState } from "store/appStore";
+import { useAuthState } from "hooks/useAuthState";
+
+const RenderSearch = () => {
+  const user: TUser | null = useSelector((store: RootState) => store.user);
+  return (
+    user && <SearchComponent />
+  );
+};
 
 const Header = () => {
   console.log('Header  render');
   useDocumentTitle();
-  const user: TUser | null = useSelector((store: RootState) => store.user);
+  useAuthState();
 
-  const location = useLocation();
-  const navigate = useNavigate();
   const dispatch = useDispatch();
   
   const hbMenuRef: RefObject<HTMLDivElement> = useRef(null);
@@ -39,33 +39,6 @@ const Header = () => {
       dispatch(TOGGLE_HAMBURGER_MENU(false));
     }
   });
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const { uid, email, displayName, photoURL } = user;
-        dispatch(ADD_USER({ uid, email, displayName, photoURL }));
-        const allowedPaths = Object.values(PATHS).filter(path => path !== PATHS.AUTH && path !== PATHS.LOGIN);
-        if (allowedPaths.includes(location.pathname)) {
-          navigate(location.pathname);
-        } else {
-          navigate(PATHS.BROWSE);
-        }
-      } else {
-        dispatch(UPDATE_SEARCH_QUERY(""));
-        dispatch(LOGOUT_USER());
-      }
-    });
-
-    // When component is unmounted, unsubscribe from the listener
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-
-  useEffect(() => {
-    dispatch(UPDATE_SEARCH_QUERY(""));
-  }, [location]);
 
   return (
     <div>
@@ -83,7 +56,7 @@ const Header = () => {
         </div>
         <div className="flex items-center xs:gap-1 l:gap-3">
           <LangSelect />
-          {user && <SearchComponent />}
+          <RenderSearch />
           <ProfileDropdown />
         </div>  
       </ScrollContainer>
